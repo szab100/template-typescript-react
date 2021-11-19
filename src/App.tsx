d4d86199e1b1d5a814dda63d80632e198fe89a37
Szabolcs Fruhwald <git@szab100.com>
2021-11-19 09:00:53 +0000

    Revert "GED rocks!"
    This reverts commit d7de590a8590b20b7e10fe64f69df4b43d20cdc7.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello GED Warriors!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
